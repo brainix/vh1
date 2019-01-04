407c71e3fd2a285d7b709d8e9dd8a773d576e5db
Raj Shah <rajiv.shah@reddit.com>
2019-01-03 16:29:52 -0800

    Happy new year!

--- src/Player.test.js
+++ src/Player.test.js
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------*\
  |  Player.test.js                                                           |
  |                                                                           |
- |  Copyright © 2017-2018, Rajiv Bakulesh Shah, original author.             |
+ |  Copyright © 2017-2019, Rajiv Bakulesh Shah, original author.             |
  |                                                                           |
  |      This program is free software: you can redistribute it and/or modify |
  |      it under the terms of the GNU General Public License as published by |
