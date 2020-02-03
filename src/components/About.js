6d000980431dd38e249c031b25c7bea44d7721bd
Rajiv Bakulesh Shah <brainix@gmail.com>
2020-02-02 22:36:20 -0800

    Rewrite About as a function component

--- src/components/About.js
+++ src/components/About.js
@@ -18,48 +18,46 @@
  |          <http://www.gnu.org/licenses/>                                   |
 \*---------------------------------------------------------------------------*/
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import Player from './Player';
 import './About.css';
 
-class About extends React.PureComponent {
-  componentDidMount() {
+const About = (props) => {
+  useEffect(() => {
     document.title = 'Spool - About Me';
-  }
+  });
 
-  render() {
-    return (
-      <>
-        <Player state='background' history={this.props.history} />
-        <section className="About">
-          <h1>About Me</h1>
+  return (
+    <>
+      <Player state='background' history={props.history} />
+      <section className="About">
+        <h1>About Me</h1>
 
-          <h2>What is Spool?</h2>
-          <p>
-            <Link to='/'>Spool</Link> takes the experience of channel surfing
-            and puts it online.  I hope that you enjoy using it as much as
-            I&rsquo;ve enjoyed building it.
-          </p>
+        <h2>What is Spool?</h2>
+        <p>
+          <Link to='/'>Spool</Link> takes the experience of channel surfing and
+          puts it online.  I hope that you enjoy using it as much as I&rsquo;ve
+          enjoyed building it.
+        </p>
 
-          <h2>Just &hellip; Why?</h2>
-          <p>
-            I want to build something frivolous, but in a particular way.
-            Inexplicable like <small>MTV</small> or Snapchat.
-          </p>
+        <h2>Just &hellip; Why?</h2>
+        <p>
+          I want to build something frivolous, but in a particular way.
+          Inexplicable like <small>MTV</small> or Snapchat.
+        </p>
 
-          <h2>How can I contact you?</h2>
-          <p>
-            Find me on Reddit
-            (<a href='https://www.reddit.com/user/Brainix'>u/Brainix</a>),
-            email me
-            (<a href='mailto:brainix@gmail.com'>brainix@gmail.com</a>),
-            or have coffee with me any time in San Francisco.
-          </p>
-        </section>
-      </>
-    );
-  }
-}
+        <h2>How can I contact you?</h2>
+        <p>
+          Find me on Reddit
+          (<a href='https://www.reddit.com/user/Brainix'>u/Brainix</a>),
+          email me
+          (<a href='mailto:brainix@gmail.com'>brainix@gmail.com</a>),
+          or have coffee with me any time in San Francisco.
+        </p>
+      </section>
+    </>
+  );
+};
 
 export default withRouter(About);
